fee039593f09a73b6a96c631dd6cd11ab7f4c372
pancham <panchamrb@phonepe.com>
2018-01-22 18:07:40 +0530

    Added more tests and functionality
    Can create instance of Checkout class
    Can add item price
    Can add an item
    Can calculate the current total
    Can add multiple items and get correct total
    Can add discount rules

--- Checkout.py
+++ Checkout.py
@@ -1,2 +1,17 @@
 class Checkout:
-    pass
+
+    def __init__(self):
+        self.prices = {}
+        self.total = 0
+
+    def addDiscount(self, item, nbrOfItems, price):
+        pass
+
+    def addItemPrice(self, item, price):
+        self.prices[item] = price
+
+    def addItem(self, item):
+        self.total +=  self.prices[item]
+
+    def calculateTotal(self):
+        return self.total
--- TestCheckout.py
+++ TestCheckout.py
@@ -1,4 +1,21 @@
+import pytest
 from Checkout import Checkout
 
-def test_CanInstantiateCheckout():
-    co = Checkout()
+@pytest.fixture()
+def checkout():
+    checkout = Checkout()
+    checkout.addItemPrice("a", 1)
+    checkout.addItemPrice("b", 2)
+    return checkout
+
+def test_CanCalculateTotal(checkout):
+    checkout.addItem("a")
+    assert checkout.calculateTotal() == 1
+
+def test_GetCorrectTotalWithMultipleItems(checkout):
+    checkout.addItem("a")
+    checkout.addItem("b")
+    assert checkout.calculateTotal() == 3
+
+def test_CanAddDiscountRule(checkout):
+    checkout.addDiscount("a", 3, 2)
